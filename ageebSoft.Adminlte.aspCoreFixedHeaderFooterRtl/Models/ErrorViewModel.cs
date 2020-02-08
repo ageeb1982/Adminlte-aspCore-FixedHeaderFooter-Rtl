fc40e87a1798beb0a19ab2d35bd22c42b713097a
Mohamed Khidir Ageeb <ageeb1982@gmail.com>
2020-02-08 10:06:20 +0300

    Add project files.

---
+++ ageebSoft.Adminlte.aspCoreFixedHeaderFooterRtl/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ageebSoft.Adminlte.aspCoreFixedHeaderFooterRtl.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
